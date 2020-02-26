039411b494582fbff84327a1a5782bbcc8af53f1
Rachid ETTABAAI <rachid.ettabaai.simplon@gmail.com>
2020-02-26 16:12:52 +0100

    modification in index.php

--- index.php
+++ index.php
@@ -80,13 +80,17 @@
             </li>
         </ul>
         <hr/>
-        <form method="POST" action="">
-            <label for="search">Rechercher :</label>
-            <input type="text" name="search" />
-            <button type="submit" class="btn btn-primary">Recherche</button>
-        </form>
 
-       
+        <section>
+
+            <form method="POST" action="">
+                <label for="search">Rechercher :</label>
+                <input type="text" name="search" />
+                <button type="submit" class="btn btn-primary">Recherche</button>
+            </form>
+
+        </section>
+
     </body>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
